145af84f9ce2b9bc8bd7c9b65f0fd78e2c1f4a9e
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-01-14 17:48:19 +0100

    Update main.c

--- kernel/main.c
+++ kernel/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
 void kernel_entry( void ) {
+    /** This is the main init entrypoint
+      * while starting up after switching to
+      * the kernel mode. */
+    char *greet = "Welcome to the RoseOS v0.0.1 build 2021.1\n\nHello, world!\n\n";
 
 }
