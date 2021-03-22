3e003e138760f74c0b6eb0be196f08e8e5c1bab9
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-03-22 10:41:07 +0100

    Delete lib.h

--- include/lib.h
+++
@@ -1,8 +0,0 @@
-extern message M;
-
-#define MM                 0
-#define FS                 1
-
-extern int callm1(), callm3(), callx(), len();
-extern int errno;
-extern int begsig();		/* interrupts all vector here */
