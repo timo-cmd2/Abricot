afaf48f7c044a7f3763ee44fc81e5fa1d1b4d81e
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-03-22 10:37:36 +0100

    Delete main.c

--- kernel/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void kernel_entry( void ) {
-    /* This is the main init entrypoint
-     * while starting up after switching to the kernel mode. */
-    char *greet = "Welcome to the RoseOS v0.0.1 build 2021.1\n\nHello, world!\n\n";
-
-}
